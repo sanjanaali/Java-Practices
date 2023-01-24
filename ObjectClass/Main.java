9f928f06f92ec9bef12d73c099e23a6f440c0d78
sanjanaali <51731623+sanjanaali@users.noreply.github.com>
2023-01-24 21:23:28 +0600

    Add files via upload

--- ObjectClass/Main.java
+++ ObjectClass/Main.java
@@ -1,9 +1,9 @@
 package com.mycompany.objectclass;
-
+import java.util.Scanner;
 public class Main {
     
     private static Table table = new Table();
-    private static Chaos chaos = new Chaos();//create the class with multiple arguments
+    private static Chaos chaos = new Chaos();
      
     public static void main(String[] args){
         
@@ -13,13 +13,22 @@ public class Main {
          System.out.println("Table leg = " +table.numberOfLegs );
          table. weight = 20.05;
          table.Weight();
+         System.out.println();
          
-         chaos.name = "ABC";
-         chaos.Name();
-         chaos.age = 20;
-         chaos.Age();
-         chaos. weight = 30.15;
-         chaos.Weight();        
+         System.out.println("Enter Chaos Name: " );
+         Scanner n = new Scanner(System.in);
+         chaos.name = n.nextLine();
+         
+         System.out.println("Enter Chaos Age: " );
+         Scanner a = new Scanner(System.in);
+         chaos.age = a.nextInt();
+         
+         System.out.println("Enter Chaos Weight: " );
+         Scanner w = new Scanner(System.in);
+         chaos. weight = w.nextDouble();
+         
+         System.out.println();
+         chaos.Name();chaos.Age();chaos.Weight();        
     }
     
 }
